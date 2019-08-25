37ca2560891db6be630d7072cee6dadb132cc2ec
Anton <attim38@gmail.com>
2019-08-25 22:08:11 +0700

    Update IAsyncMessage.cs

--- TaskDialogWrapper/TaskDialogWrapper/TaskDialogAsyncMessageWrappers/IAsyncMessage.cs
+++ TaskDialogWrapper/TaskDialogWrapper/TaskDialogAsyncMessageWrappers/IAsyncMessage.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace TaskDialogWrapper
 {
+    /// <summary>
+    /// A interface for messages to the Task Dialog.
+    /// </summary>
     internal interface IAsyncMessage
     {
+        /// <summary>
+        /// Specifies the operation signature that will be called and executed in the Task Dialog callback.
+        /// </summary>
         int Execute(IntPtr handle);
     }
 }
